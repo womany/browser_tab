9f56346fcb9e196823de94f14cc97bdd4e9d052f
wildskyf <poppin.wildsky@gmail.com>
2016-07-05 11:50:33 +0800

    Add latest articles list

---
+++ js/latest-articles.js
@@ -0,0 +1,17 @@
+fetch("https://api.womany.net/articles/list")
+	.then( res => res.json())
+	.then( ({articles}) => {
+		let latest_ul = document.querySelector("#latest-articles > ul");
+		var content = "";
+		let article_limit_num = 6;
+		for(var i = 0; i < article_limit_num ; ++i ) {
+			content += `<li>
+			<a href="https://womany.net/read/article/${articles[i].id}?ref=ext-chrome-"
+				style="background-image:url('https://${articles[i].featured_image_thumb}')">
+				<h3>${articles[i].title}</h3>
+			</a></li>`
+		}
+		latest_ul.innerHTML = content;
+	})
+	.catch( err => {console.log(err)})
+
