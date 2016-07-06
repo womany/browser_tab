4b767e067042731cd7f27a8d4acbb0d35e62132d
wildskyf <poppin.wildsky@gmail.com>
2016-07-06 16:41:08 +0800

    Add search suggestion

--- js/time.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-	var time = document.querySelector(".time span");
-	time.innerHTML = moment().locale("zh_TW").format("M/D, h:mm:ss");
-	window.setInterval( () => {
-		time.innerHTML = moment().locale("zh_TW").format("M/D, h:mm:ss");
-	}, 1000);
-}
